f28f705ed7111ca19656996a6b44dd092d248328
srtipu5 <srtipu5@gmail.com>
2021-11-09 23:21:42 +0600

    first

---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+        public string EmployeeFirstName
+        {
+            get;
+            set;
+        }
+        public string EmployeeLastName
+        {
+            get;
+            set;
+        }
+        public decimal Salary
+        {
+            get;
+            set;
+        }
+        public string Designation
+        {
+            get;
+            set;
+        }
+    }
+}
